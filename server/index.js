07bbff0ed47fa656cc7e8e802ce9ad0bfcc9b9a3
sulthan441 <mohammedsulthan441@gmail.com>
2023-03-21 00:28:30 +0530

    backend done

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const connect = require('./db/connect');
+const connect = require("./db/connect");
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
-app.use(express.json());
-const port = 8080;
 
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.static('build'))
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productsRoutes');
+const cartRouter = require('./routes/cartRoutes')
+const port = process.env.PORT || 8080;
+
+app.use('/api', userRouter);
+app.use('/api', productRouter);
+app.use('/api', cartRouter)
+app.use('/api/*', (req, res) => {
+    return res.status(404).send(' page not found')
+});
 
 connect().then(() => {
     app.listen(port, () => {
-        console.log(`server listening on port ${port}`);
+        console.log(`server running on the port:${port}`)
     })
 })
