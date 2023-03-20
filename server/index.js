b4574c0b0a5bf841d296d398c83405311da0bfdd
sulthan441 <mohammedsulthan441@gmail.com>
2023-03-20 22:52:31 +0530

    user db

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port =  8080;
+app.listen(port,()=>{
+    console.log(`server listening on port ${port}`);
+})
