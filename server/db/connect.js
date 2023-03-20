2572883f5b6b4a162052cb35f55d1656d8d2d33f
sulthan441 <mohammedsulthan441@gmail.com>
2023-03-20 23:19:09 +0530

    database connected succesfully

--- server/db/connect.js
+++ server/db/connect.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const config = require('../config/')
 async function connect(){
     return new Promise((res,rej)=>{
-        mongoose.connect()
+        mongoose.connect(config.DB_CONNECTION_URL,(err)=>{
+            if(err){
+                console.log("error occured in connecting database")
+                return rej(err)
+            }
+            console.log("database connected succesfully")
+            res();
+        })
     })
 }
+module.exports = connect;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-
+const connect = require('./db/connect');
 const app = express();
 app.use(express.json());
-const port =  8080;
-app.listen(port,()=>{
-    console.log(`server listening on port ${port}`);
+const port = 8080;
+
+
+connect().then(() => {
+    app.listen(port, () => {
+        console.log(`server listening on port ${port}`);
+    })
 })
